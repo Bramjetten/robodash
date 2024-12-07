1e47c69a6a7c5ae5daf9a5d98e095f22b914f50d
Bram Jetten <mail@bramjetten.nl>
2024-12-07 10:18:23 +0100

    Dashboard widgets comments'

--- app/models/dashboard.rb
+++ app/models/dashboard.rb
@@ -5,6 +5,10 @@ class Dashboard < ApplicationRecord
   has_secure_token :token
 
   # A dashboard is down if any of its widgets are down
+  # This is not cached and potentially quite slow
+  #
+  # Could be more efficient, but would require knowledge 
+  # of all existing widgetables. Not great design.
   def down?
     widgets.any?(&:down?)
   end
