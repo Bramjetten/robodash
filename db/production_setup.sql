f668519c213caab918bbdb7371a86c614e197624
Bram Jetten <mail@bramjetten.nl>
2024-12-19 15:14:47 +0100

    Add postgres as accessory

---
+++ db/production_setup.sql
@@ -0,0 +1,6 @@
+-- db/production_setup.sql
+ 
+CREATE DATABASE robodash_production;
+CREATE DATABASE robodash_production_cache;
+CREATE DATABASE robodash_production_queue;
+CREATE DATABASE robodash_production_cable;
