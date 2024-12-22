6f4d0e7a7b9db680edf88522fe639801ad2f084c
Bram Jetten <mail@bramjetten.nl>
2024-12-22 22:14:32 +0100

    Show dash

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,7 +1,7 @@
 class DashboardsController < ApplicationController
 
   def show
-    @dashboard = Dashboard.find(params[:id])
+    @dashboard = Dashboard.joins(:account).where(account: Current.user.accounts).find(params[:id])
   end
 
 end
