88e3e22fb48b531675e31651270681e838feb115
Bram Jetten <mail@bramjetten.nl>
2024-12-22 14:32:24 +0100

    Application pwa and icons

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,6 +2,8 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
+  config.hosts << "robodash.puma"
+  config.hosts << "robodash.192.168.1.216.nip.io"
 
   # Make code changes take effect immediately without server restart.
   config.enable_reloading = true
