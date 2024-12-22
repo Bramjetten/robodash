5e8027559c24d31e8f948e5a6631e1c948a3f023
Bram Jetten <mail@bramjetten.nl>
2024-12-22 22:15:52 +0100

    Dashboards

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 class DashboardsController < ApplicationController
 
   def show
+    # TODO: add Current.account and get the dashboard using: 
+    # Current.account.dashboards.find(params[:id])
     @dashboard = Dashboard.joins(:account).where(account: Current.user.accounts).find(params[:id])
   end
 
