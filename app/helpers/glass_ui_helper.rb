953ade5bc92ee10120b0169027d153f7a27e492d
Bram Jetten <mail@bramjetten.nl>
2024-12-27 11:17:39 +0100

    Tweaking of UI

--- app/helpers/glass_ui_helper.rb
+++ app/helpers/glass_ui_helper.rb
@@ -1,8 +1,8 @@
 module GlassUIHelper
 
-  def glass_well
-    content_tag(:div, class: "bg-gradient-to-br from-zinc-200/60 from-10% via-30% via-zinc-400/20 to-zinc-400/20 backdrop-blur-sm rounded-[14px] p-[1.5px] shadow-lg shadow-black/30") do
-      content_tag(:div, class: "bg-zinc-900/50 rounded-[13px] px-3 py-2") do
+  def glass_well(broken: false)
+    content_tag(:div, class: "bg-gradient-to-br from-zinc-200/40 from-10% via-30% via-zinc-400/20 to-zinc-400/20 backdrop-blur-sm rounded-[14px] p-[1.5px] shadow-lg shadow-black/30 relative overflow-hidden") do
+      content_tag(:div, class: "bg-zinc-900/50 rounded-[13px] px-3 py-2 bg-cover bg-no-repeat bg-right", style: ("background-image: url(#{image_url('broken-glass.webp')})" if broken)) do
         yield
       end
     end
@@ -16,6 +16,12 @@ module GlassUIHelper
         bg-gradient-to-br from-cyan-200 from-10% via-30% via-cyan-300/90 to-cyan-300/80
         shadow-lg shadow-black/60
         drop-shadow-[0_1.5px_0px_theme(colors.cyan.700)]
+      ]
+    elsif style == "danger"
+      classes.concat %w[
+        bg-gradient-to-br from-red-300 from-10% via-30% via-red-400/90 to-red-400/80
+        shadow-lg shadow-black/60
+        drop-shadow-[0_1.5px_0px_theme(colors.red.700)]
       ]
     else
       classes.concat %w[
@@ -32,6 +38,8 @@ module GlassUIHelper
 
     if style == "primary"
       classes.concat %w[bg-gradient-to-b from-cyan-500 to-cyan-600]
+    elsif style == "danger"
+      classes.concat %w[bg-gradient-to-b from-red-600 to-red-700]
     else
       classes.concat %w[bg-zinc-800/50]
     end
@@ -54,6 +62,9 @@ module GlassUIHelper
     # Primary style
     if glass_style == "primary"
       container_classes = "shadow-xl shadow-cyan-500/30 rounded-full" 
+      inner_shadow = "box-shadow: inset 0 0 10px rgba(255, 255, 255, .4)"
+    elsif glass_style == "danger"
+      container_classes = "shadow-xl shadow-red-500/30 rounded-full" 
       inner_shadow = "box-shadow: inset 0 0 10px rgba(255, 255, 255, .4)"
     end
 
