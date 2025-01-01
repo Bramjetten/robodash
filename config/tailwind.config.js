d7f54d2ac147900801417f2951a29bdb3adaf9e9
Bram Jetten <mail@bramjetten.nl>
2025-01-01 10:29:43 +0100

    TX-02 font

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -11,6 +11,7 @@ module.exports = {
     extend: {
       fontFamily: {
         sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+        mono: ['TX-02', ...defaultTheme.fontFamily.mono]
       },
     },
   },
