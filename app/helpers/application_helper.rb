36d5afd214904a0cf8e342798f335aafce8fa8bd
Bram Jetten <mail@bramjetten.nl>
2025-06-29 10:35:02 +0200

    Navigation

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,12 @@ module ApplicationHelper
     wallpapers.find{_1 == cookies[:wallpaper].to_i} || 3
   end
 
+  def github_url
+    "https://github.com/bramjetten/robodash"
+  end
+
+  def gem_url
+    "https://github.com/Bramjetten/robodash-gem"
+  end
+
 end
