d881f1412b739afc776ff1f5de18aed9df65893b
Bram Jetten <mail@bramjetten.nl>
2025-06-30 12:23:55 +0200

    Lowercase

--- app/helpers/heartbeats_helper.rb
+++ app/helpers/heartbeats_helper.rb
@@ -3,12 +3,12 @@ module HeartbeatsHelper
   def heartbeat_schedule(heartbeat)
     if heartbeat.schedule_number == 1
       if heartbeat.schedule_period == "day"
-        "Daily"
+        "daily"
       else
         "#{heartbeat.schedule_period}ly"
       end
     else
-      "Every #{heartbeat.schedule_number} #{heartbeat.schedule_period.pluralize}"
+      "every #{heartbeat.schedule_number} #{heartbeat.schedule_period.pluralize}"
     end
   end
 
