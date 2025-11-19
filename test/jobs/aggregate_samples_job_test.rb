9297b9d0fea7bf521a4d70e7f0558715d7063b5b
Nick Belzer <nbelzer@hey.com>
2025-11-19 08:43:59 +0100

    Aggregate samples by min, median, and max

--- test/jobs/aggregate_samples_job_test.rb
+++ test/jobs/aggregate_samples_job_test.rb
@@ -1,7 +1,7 @@
 require "test_helper"
 
 class AggregateSamplesJobTest < ActiveJob::TestCase
-  test "aggregates older samples together by max value" do
+  test "aggregates older samples only" do
     travel_to Time.current do
       measurement = measurements(:puma_backlog)
 
@@ -17,7 +17,28 @@ class AggregateSamplesJobTest < ActiveJob::TestCase
       end
 
       assert_equal 61.minutes.ago.beginning_of_minute, measurement.samples.first.timestamp
-      assert_equal 1, measurement.samples.first.value
+      assert_equal 1, measurement.samples.first.max
+    end
+  end
+
+  test "correctly aggregates the min, median, and max values" do
+    travel_to Time.current do
+      measurement = measurements(:puma_backlog)
+
+      measurement.samples.create!(timestamp: 61.minutes.ago, value: 4)
+      measurement.samples.create!(timestamp: 61.minutes.ago, value: 3)
+      measurement.samples.create!(timestamp: 61.minutes.ago, value: 1)
+
+      perform_enqueued_jobs do
+        assert_difference("Sample.count", -2) do
+          AggregateSamplesJob.perform_later
+        end
+      end
+
+      assert_equal 61.minutes.ago.beginning_of_minute, measurement.samples.first.timestamp
+      assert_equal 1, measurement.samples.first.min
+      assert_equal 3, measurement.samples.first.value
+      assert_equal 4, measurement.samples.first.max
     end
   end
 end
