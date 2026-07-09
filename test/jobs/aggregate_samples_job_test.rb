9b6142cffec650a4c86646c1b8c18ba28f3f5b5d
Nick Belzer <nbelzer@hey.com>
2026-07-09 11:02:54 +0200

    Aggregate samples by 20 minutes
    For the size of our graphs, aggregates make more sense over buckets of
    10 minutes. Also aggregate up until the last possible bucket

--- test/jobs/aggregate_samples_job_test.rb
+++ test/jobs/aggregate_samples_job_test.rb
@@ -1,44 +1,67 @@
 require "test_helper"
 
 class AggregateSamplesJobTest < ActiveJob::TestCase
-  test "aggregates older samples only" do
-    travel_to Time.current do
-      measurement = measurements(:puma_backlog)
+  setup do
+    @bucket = AggregateSamplesJob::BUCKET
+    travel_to Time.current.beginning_of_hour + @bucket / 2
+  end
 
-      measurement.samples.create!(timestamp: 61.minutes.ago)
-      measurement.samples.create!(timestamp: 61.minutes.ago, value: 1)
-      measurement.samples.create!(timestamp: 60.minutes.ago)
-      measurement.samples.create!(timestamp: 59.minutes.ago)
+  test "aggregates completed buckets only" do
+    measurement = measurements(:puma_backlog)
 
-      perform_enqueued_jobs do
-        assert_difference("Sample.count", -1) do
-          AggregateSamplesJob.perform_later
-        end
+    # To be aggregated
+    measurement.samples.create!(timestamp: (@bucket + 5.minutes).ago)
+    measurement.samples.create!(timestamp: (@bucket + 4.minutes).ago, value: 1)
+
+    # Within the latest window
+    measurement.samples.create!(timestamp: 5.minutes.ago)
+    measurement.samples.create!(timestamp: 4.minutes.ago)
+
+    perform_enqueued_jobs do
+      assert_difference("Sample.count", -1) do
+        AggregateSamplesJob.perform_later
       end
-
-      assert_equal 61.minutes.ago.beginning_of_minute, measurement.samples.first.timestamp
-      assert_equal 1, measurement.samples.first.max
     end
+
+    aggregated = measurement.samples.first
+    assert_equal 30.minutes.ago, aggregated.timestamp
+    assert_equal 1, aggregated.max
+
+    # The open bucket keeps its raw samples.
+    assert_equal 2, measurement.samples.where(min: nil).count
   end
 
   test "correctly aggregates the min, median, and max values" do
-    travel_to Time.current do
-      measurement = measurements(:puma_backlog)
+    measurement = measurements(:puma_backlog)
 
-      measurement.samples.create!(timestamp: 61.minutes.ago, value: 4)
-      measurement.samples.create!(timestamp: 61.minutes.ago, value: 3)
-      measurement.samples.create!(timestamp: 61.minutes.ago, value: 1)
+    measurement.samples.create!(timestamp: (@bucket + 5.minutes).ago, value: 4)
+    measurement.samples.create!(timestamp: (@bucket + 4.minutes).ago, value: 3)
+    measurement.samples.create!(timestamp: (@bucket + 3.minutes).ago, value: 1)
 
-      perform_enqueued_jobs do
-        assert_difference("Sample.count", -2) do
-          AggregateSamplesJob.perform_later
-        end
+    perform_enqueued_jobs do
+      assert_difference("Sample.count", -2) do
+        AggregateSamplesJob.perform_later
       end
-
-      assert_equal 61.minutes.ago.beginning_of_minute, measurement.samples.first.timestamp
-      assert_equal 1, measurement.samples.first.min
-      assert_equal 3, measurement.samples.first.value
-      assert_equal 4, measurement.samples.first.max
     end
+
+    aggregated = measurement.samples.first
+    assert_equal 30.minutes.ago, aggregated.timestamp
+    assert_equal 1, aggregated.min
+    assert_equal 3, aggregated.value
+    assert_equal 4, aggregated.max
+  end
+
+  test "does not aggregate buckets with a single sample" do
+    measurement = measurements(:puma_backlog)
+
+    measurement.samples.create!(timestamp: (@bucket + 5.minutes).ago, value: 4)
+
+    perform_enqueued_jobs do
+      assert_no_difference("Sample.count") do
+        AggregateSamplesJob.perform_later
+      end
+    end
+
+    assert_nil measurement.samples.first.min
   end
 end
